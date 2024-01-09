3ec298c1299cea8f415da0177e22c21f04ec4b47
Javier <javier.casas@3xmgroup.com>
2024-01-09 06:25:20 -0300

    small fix + formatting

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -4,28 +4,27 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 const INCREASE_VALUE = 1;
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'increase_counter':
-            return {...state, counter: state.counter + action.payload };
-        case 'decrease_counter':
-            return {...state, counter: state.counter - action.payload };
-        default: 
-            return state;
-    }
+	switch (action.type) {
+		case 'increase_counter':
+			return { ...state, counter: state.counter + action.payload };
+		case 'decrease_counter':
+			return { ...state, counter: state.counter - action.payload };
+		default:
+			return state;
+	}
 };
 
-
 const CounterScreen = () => {
-    const [ state, dispatch ] = useReducer(reducer, {counter:0});
+	const [state, dispatch] = useReducer(reducer, { counter: 0 });
 
-return (
-    <View>
-        <Button title='Increase' onPress={() => dispatch({type:'increase_counter', payload: INCREASE_VALUE})}/>
-        <Button title='Decrease' onPress={() => dispatch ({type:'decrease_counter', payload: INCREASE_VALUE})} />
-        <Text style={styles.text}>Current Count: {counter}</Text>
-    </View>
-);
-}
+	return (
+		<View>
+			<Button title="Increase" onPress={() => dispatch({ type: 'increase_counter', payload: INCREASE_VALUE })} />
+			<Button title="Decrease" onPress={() => dispatch({ type: 'decrease_counter', payload: INCREASE_VALUE })} />
+			<Text style={styles.text}>Current Count: {state.counter}</Text>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
 	text: {
